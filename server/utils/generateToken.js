d9e0a4f89a4461bf4198e19bd88e463549bf8ea7
ritikaraikwar-dev <ritikaraikwar24@gmail.com>
2025-08-26 13:33:31 +0530

    geneate token and cookie

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,11 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId)=>{
-   return  jwt.sign(
+   const token = jwt.sign(
         { id:userId},
     process.env.SECRET_KEY,
     {expireIn:"7d"}
-    )   
+    ) 
+
+    res.cookie("jwt",token , {
+      httpOnly:true,
+      secure:true,
+      sameSite:"none",
+      maxAge:7*24*60*60*1000
+    }); 
+    return token;
 }
+
+ 
 
 module.exports = generateToken;
